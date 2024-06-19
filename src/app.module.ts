4ab24683c3efa9a8d9e6ed021026f7c51a5b4beb
rmcampigotto <rmcampigotto@gmail.com>
2024-06-19 00:52:56 -0300

    Iniciando a configuração do projeto | 19/06

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { CharactersModule } from './characters/characters.module';
+
+@Module({
+  imports: [CharactersModule],
+  controllers: [],
+  providers: [],
+})
+
+export class AppModule {}
